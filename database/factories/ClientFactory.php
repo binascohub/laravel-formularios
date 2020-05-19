310f7dd94fe68a9e46e500ccdd7fd633aa27a16e
binascohub <fernando.binasco@gmail.com>
2020-05-18 23:15:27 -0300

    adicionando factory faker para model Client

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Client;
+use Faker\Generator as Faker;
+
+$factory->define(Client::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'defaulter' => rand(0,1),
+        'date_birth' => $faker->date(),
+        'sex' => rand(1,10)%2==0?'m':'f',
+        'marital_status' => rand(1,3),
+        'physical_disability' => rand(1,10) % 2 == 0 ? $faker->word : null
+    ];
+});
