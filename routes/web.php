bc62464a52a875126127fee241083b439a76a885
binascohub <fernando.binasco@gmail.com>
2020-05-20 00:01:41 -0300

    adicionando controller e rotas com resources

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    Route::resource('clients', 'ClientsController');
+});
